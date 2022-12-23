4257e19f269f0d759dff5434a29d6b2f123e9d92
AndrewVorotyntsev <andrey.vortyntsev@mail.ru>
2022-12-23 09:47:42 +0300

    отдельный компонент для навигации

---
+++ src/components/NavigationMenuComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-menu class="el-menu-demo" mode="horizontal" :router="true"
+           background-color="#adacf5"
+           text-color="#fff"
+           active-text-color="#6000ff">
+    <el-menu-item index="1" :route="{ name: 'home' }">Home</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">Places</template>
+      <el-menu-item index="2-1" :route="{ name: 'places', params: { category: 'mountain' } }">Горы</el-menu-item>
+      <el-menu-item index="2-2" :route="{ name: 'places', params: { category: 'forest' } }">Леса</el-menu-item>
+      <el-menu-item index="2-3" :route="{ name: 'places', params: { category: 'river' } }">Реки</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="3" :route="{ name: 'favorites' }">Favorites</el-menu-item>
+    <el-menu-item index="4" :route="{ name: 'about' }">About</el-menu-item>
+  </el-menu>
+</template>
+<script>
+export default {
+  name: 'NavigationMenuComponent'
+}
+</script>
--- src/components/layout/PageLayout.vue
+++ src/components/layout/PageLayout.vue
@@ -2,20 +2,7 @@
     <div class="page-layout">
         <header>
             <slot name="header">
-                <el-menu class="el-menu-demo" mode="horizontal" :router="true"
-                         background-color="#adacf5"
-                         text-color="#fff"
-                         active-text-color="#6000ff">
-                    <el-menu-item index="1" :route="{ name: 'home' }">Home</el-menu-item>
-                    <el-submenu index="2">
-                        <template slot="title">Places</template>
-                        <el-menu-item index="2-1" :route="{ name: 'places', params: { category: 'mountain' } }">Горы</el-menu-item>
-                        <el-menu-item index="2-2" :route="{ name: 'places', params: { category: 'forest' } }">Леса</el-menu-item>
-                      <el-menu-item index="2-3" :route="{ name: 'places', params: { category: 'river' } }">Реки</el-menu-item>
-                    </el-submenu>
-                    <el-menu-item index="3" :route="{ name: 'favorites' }">Favorites</el-menu-item>
-                    <el-menu-item index="4" :route="{ name: 'about' }">About</el-menu-item>
-                </el-menu>
+              <NavigationMenuComponent/>
             </slot>
         </header>
         <slot>Не найдено</slot>
@@ -23,10 +10,12 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import {mapGetters} from 'vuex';
+import NavigationMenuComponent from "@/components/NavigationMenuComponent.vue";
 
 export default {
     name: 'PageLayout',
+  components: {NavigationMenuComponent},
     computed: {
         ...mapGetters([
             'getListBasket'
